a7b0788584ba0db2b7263fb79bf6c64a8ea5570f
Gabriel Castro <132520554+ThatFireBoi@users.noreply.github.com>
2023-12-21 12:08:05 -0400

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """ Blueprint for API """
-from api.v1.views.index import *
 from flask import Blueprint
+
+# Create Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard imports
+from api.v1.views.index import *
