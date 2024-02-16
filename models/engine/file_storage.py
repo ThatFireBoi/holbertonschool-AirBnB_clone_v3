282a3f2a58c7e142eb1b444815439e9ca2815e0b
ThatFireBoi <gcf2007@hotmail.com>
2024-02-15 20:33:30 -0400

    Correction to delete function, in regards to function when the object is equal to None

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,7 +60,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
+        """Delete obj from __objects if it’s inside - if obj is equal to None, the method should not do anything"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
