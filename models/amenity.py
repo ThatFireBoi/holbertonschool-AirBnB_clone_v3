3a78be89a5bd0e4fd3ce0e5e4485b7b15630a520
ThatFireBoi <gcf2007@hotmail.com>
2024-02-15 20:39:02 -0400

    Updates to comments for Amenity class, and correction of else statement

--- models/amenity.py
+++ models/amenity.py
@@ -3,13 +3,12 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+    """ Amentity class to store information about amenities """
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
